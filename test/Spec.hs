9043e6914d8af3811317b94a4b2a356708c8f60d
Nicolas <nicolaseven76@gmail.com>
2021-01-23 20:06:01 +0100

    (feat) adding

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,18 @@
+import Test.Hspec
+import Network.HTTP.Conduit
+import qualified Data.ByteString.Lazy as B (toStrict)
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    image_png <- simpleHttp "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Haskell-Logo.svg/1200px-Haskell-Logo.svg.png"
+    image_jpg <- simpleHttp "https://d1fmx1rbmqrxrr.cloudfront.net/cnet/optim/i/edit/2019/04/eso1644bsmall__w770.jpg"
+    image_jpg2 <- simpleHttp "https://blog-fr.orson.io/wp-content/uploads/2017/06/jpeg-ou-png.jpg"
+    hspec $
+      describe "check image type" $ do
+        it "should be a png" $
+          matchBytestringTypes (B.toStrict image_png) `shouldBe` ["png"]
+        it "should be a jpeg" $
+          matchBytestringTypes (B.toStrict image_jpg) `shouldBe` ["jpeg"]
+        it "should be a png" $
+          matchBytestringTypes (B.toStrict image_png) `shouldBe` ["png"]
